5b9b6c511bb4a9d98156b9bd83f4d5ecb276478f
Marc Teo Ong <mrc.teoong@gmail.com>
2022-06-01 03:46:27 +0800

    done

---
+++ src/Components/Home.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react'; 
+import './Home.css';
+import {checkStatus, returnCount} from './Mint';
+import goblin from './goblin.gif'
+
+class Home extends Component {
+  state = {
+    count: 1,
+  }
+
+  increment = () => {
+    this.setState((state) => {
+      if(state.count < 10) {
+        checkStatus(document.getElementsByClassName('mint')[0], Number(document.getElementsByClassName("num")[0].value) + 1);
+        return {count: state.count + 1}
+      } else {
+        return {count: state.count}
+      }
+    });
+  }
+
+  decrement = () => {
+    this.setState((state) => {
+      if(state.count > 1) {
+        checkStatus(document.getElementsByClassName('mint')[0], Number(document.getElementsByClassName("num")[0].value ) - 1);
+        return {count: state.count - 1}
+      } else {
+        return {count: state.count}
+      }
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({state: event.target.value});
+  }
+
+  componentDidMount() {
+    const mintButton = document.getElementsByClassName('mint')[0];
+    const amount = Number(document.getElementsByClassName("num")[0].value);
+    const minted = document.getElementsByClassName('sold')[0];
+
+    returnCount(minted);
+    checkStatus(mintButton, amount);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="body">
+        <div className="minter">
+          <div className="mint-right">
+            <img src={goblin} className="nootimg" />
+            <h1 className="mint-title">
+              towny goblins
+            </h1>
+            </div>
+          <div className="mint-left">
+            <h1 className="mint-description">
+              1 Free. Max 10 (0.0069Ξ).
+            </h1>
+            <div className="sold">
+                /4000
+            </div>
+            <div className="mint-function">
+              <div className="mint-amt">
+                <button className="minmax" onClick={this.decrement}> - </button>
+                <input className="num" type="text" value={this.state.count} onChange={this.handleChange} readOnly ></input>
+                <button className="minmax" onClick={this.increment}> + </button>
+              </div>
+              <button className="mint">MINT</button>
+            </div>
+          </div>  
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/Components/Mint.js
@@ -0,0 +1,62 @@
+// import MetaMaskOnboarding from '/node_modules/@metamask/onboarding'
+//1. connect metamask to site, get user's address
+/* global Web3, web3, ethereum */
+
+var account = null;
+var contract = null;
+const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseExtension","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getOwnershipData","outputs":[{"components":[{"internalType":"address","name":"addr","type":"address"},{"internalType":"uint64","name":"startTimestamp","type":"uint64"}],"internalType":"struct ERC721A.TokenOwnership","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxPerTx","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"mintEnabled","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"multiplePrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nextOwnerToExplicitlySet","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"numberMinted","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"ownerBatchMint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"revealed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_newBaseExtension","type":"string"}],"name":"setBaseExtension","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_newBaseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"maxPerTx_","type":"uint256"}],"name":"setMaxPerTx","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"quantity","type":"uint256"}],"name":"setOwnersExplicit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"price_","type":"uint256"}],"name":"setPrice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"setRevealed","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"maxSupply_","type":"uint256"}],"name":"setmaxSupply","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"toggleMinting","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+const ADDRESS = "0x4799FFD4d3bBB47c6E3092537974c992783fE1c3";
+
+// const mintButton = document.getElementsByClassName('mint-button')[0];
+
+if (window.ethereum) {
+   window.web3 = new Web3(window.ethereum);
+
+   contract = new web3.eth.Contract(ABI, ADDRESS);
+
+}
+
+const onClickConnect = async (mintButton, amount) => {
+   try {
+      // Will open the MetaMask UI
+      // You should disable this button while the request is pending!
+      await ethereum.request({ method: 'eth_requestAccounts' });
+   } catch (error) {
+      console.error(error);
+   }
+   checkStatus(mintButton, amount);
+};
+
+const checkStatus = async (mintButton, amount) => {
+   var accounts = await web3.eth.getAccounts();
+   account = accounts[0]
+   if (accounts.length == 0){
+      mintButton.innerText = 'Connect Wallet';
+      mintButton.onclick = () => {
+            onClickConnect(mintButton, amount);
+         }
+   } else {
+      mintButton.innerText = 'MINT';
+      mintButton.onclick = () => {
+         contract.methods.numberMinted(account).call().then(function(result) {
+            if (Number(result) < 1) {
+               if (amount == 1) {
+                  contract.methods.mint(amount).send({ from: account, value : '0'});
+               } else {
+                  contract.methods.mint(amount).send({ from: account, value : ((amount - 1) * 6900000000000000).toString()});
+               }
+            } else {
+               contract.methods.mint(amount).send({ from: account, value : ((amount - 1) * 6900000000000000).toString()});
+            }
+         })
+         }
+   }
+}
+
+const returnCount = async (minted) => {
+   contract.methods.totalSupply().call().then(function(result) {
+      minted.innerHTML = result + "/1999";
+   })
+}
+
+export {checkStatus, returnCount};
